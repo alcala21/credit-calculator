21d29bb7ba7c6bd8de83ed5579f75041651b93d6
Carlos Alcala <alcala21@gmail.com>
2020-08-21 14:29:38 -0500

    Stage 2 done.

--- creditcalc.py
+++ creditcalc.py
@@ -1,12 +1,20 @@
-credit_principal = 'Credit principal: 1000'
-final_output = 'The credit has been repaid!'
-first_month = 'Month 1: paid out 250'
-second_month = 'Month 2: paid out 250'
-third_month = 'Month 3: paid out 500'
+import math
 
-# write your code here
-print(credit_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+principal = int(input("Enter the credit principal:\n"))
+operation = input("""What do you want to calculate?
+type \"m\" - for the number of months,
+type \"p\" - for the monthly payment:\n""")
+
+if operation == "m":
+    pay = int(input("Enter the monthly payment:\n"))
+    months = math.ceil(principal / pay)
+    print(f"It takes {months} {'month' if months == 1 else 'months'} to repay the credit.")
+
+elif operation == "p":
+    months = int(input("Enter the count of months:\n"))
+    pay = math.ceil(principal / months)
+    message = f"Your monthly payment = {pay}"
+    if principal % months != 0:
+        remaining = principal - (months - 1) * pay
+        message += f" with last monthly payment = {remaining}."
+    print(message)
