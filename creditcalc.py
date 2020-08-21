226c089d11c13289095b4d24587d001e5a8a214f
Carlos Alcala <alcala21@gmail.com>
2020-08-21 16:18:48 -0500

    Stage 3 done.

--- creditcalc.py
+++ creditcalc.py
@@ -1,20 +1,46 @@
 import math
 
-principal = int(input("Enter the credit principal:\n"))
-operation = input("""What do you want to calculate?
-type \"m\" - for the number of months,
-type \"p\" - for the monthly payment:\n""")
+parameters = ['p', 'a', 'n', 'i']
+param_dict = {'p': "Enter the credit principal:\n",
+              'a': "Enter the monthly payment:\n",
+              'n': "Enter the number of periods:\n",
+              'i': "Enter the credit interest:\n"}
 
-if operation == "m":
-    pay = int(input("Enter the monthly payment:\n"))
-    months = math.ceil(principal / pay)
-    print(f"It takes {months} {'month' if months == 1 else 'months'} to repay the credit.")
+message_dict = {'p': "Your credit principal",
+                'a': "Your annuity payment"}
 
-elif operation == "p":
-    months = int(input("Enter the count of months:\n"))
-    pay = math.ceil(principal / months)
-    message = f"Your monthly payment = {pay}"
-    if principal % months != 0:
-        remaining = principal - (months - 1) * pay
-        message += f" with last monthly payment = {remaining}."
-    print(message)
+
+def factor_value(n, i):
+    return (i * (1 + i) ** n) / ((1 + i) ** n - 1)
+
+
+param_formulas = {'p': lambda a, n, i: int(a / factor_value(n, i)),
+                  'a': lambda p, n, i: math.ceil(p * factor_value(n, i)),
+                  'n': lambda p, a, i: math.ceil(math.log((a / (a - i * p)), 1 + i))}
+
+parameter = input("""What do you want to calculate?
+type \"n\" - for the number of months,
+type \"a\" - for the annuity monthly payment,
+type \"p\" - for the credit principal:
+""")
+
+r_params = [x for x in parameters if x != parameter]
+param_values = {x: float(input(param_dict[x])) for x in r_params}
+param_values['i'] = param_values['i']/100/12
+
+value = param_formulas[parameter](*param_values.values())
+
+if parameter != 'n':
+    print(message_dict[parameter] + f" = {value}!")
+else:
+    years, months = divmod(value, 12)
+    year_str = 'year' if years == 1 else 'years'
+    month_str = 'month' if months == 1 else 'months'
+    message = ""
+    if years == 0:
+        message = f"You need {months} {month_str}"
+    elif years == 1:
+        message = f"You need {years} {year_str}"
+    elif value > 12:
+        message = f"You need {years} {year_str} and {months} {month_str}"
+    print(message + " to repay this credit!")
